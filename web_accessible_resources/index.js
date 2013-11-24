b4cd2395574f53cd81701875265dc6587571bd57
foostan <foostan27@gmail.com>
2013-11-24 16:42:11 +0900

    :golf:

--- web_accessible_resources/index.js
+++ web_accessible_resources/index.js
@@ -6,7 +6,8 @@
         '1' : {
             name : 'test',
             room_ids : [
-                7125895
+                7125895,
+                16074837
             ]
         },
         '-1' : {
@@ -19,48 +20,38 @@
         localStorage.setItem('room_filers',JSON.stringify(new_room_filters));
     }
 
-    function get_rooms() {
-        var $raw_rooms = $('#_roomListItems');
+    function overwriteRoomListItems() {
+        // overwrite all rooms
+        $('#_roomListItems li._roomLink._room')
+            .addClass("draggable")
+            .addClass("droppable");
 
-        var rooms = new Array();
+        // overwrite filtering rooms
         jQuery.each(room_filters, function(room_filter_id, category) {
-            if (room_filter_id !== OTHER_ROOM_FILTER_ID) {
-                category['room_ids'].forEach(function(room_id) {
-                    $found_room = $raw_rooms.find('li._roomLink._room[data-rid=' + room_id + ']');
-                    if ($found_room[0]) {
-                        // add elements
-                        $found_room
-                        .addClass("draggable")
-                        .addClass("droppable")
-                        .attr("room-filter-id", room_filter_id);
-                    }
-                    rooms[room_filter_id] = {
-                        //incomplete : incomplete,
-                        jq_object : $found_room
-                    };
-                });
-            } else {
-                var $other_rooms = $raw_rooms.find('li._roomLink._room').filter(function() {
-                    return $(this).attr('room-filter-id') === undefined;
-                });
-                if ($other_rooms[0]) {
-                    // add elements
-                    $other_rooms
-                    .addClass("draggable")
-                    .addClass("droppable")
-                    .attr("room-filter-id", room_filter_id);
-                }
-                rooms[OTHER_ROOM_FILTER_ID] = {
-                    //incomplete : incomplete,
-                    jq_object : $other_rooms
-                };
+            if (room_filter_id === OTHER_ROOM_FILTER_ID) {
+                return;
             }
+            category['room_ids'].forEach(function(room_id) {
+                $('#_roomListItems li._roomLink._room[data-rid=' + room_id + ']')
+                    .attr("room-filter-id", room_filter_id);
+            });
         });
 
-        console.log(rooms);
-        return rooms;
+        // overwrite other rooms
+        $('#_roomListItems li._roomLink._room')
+            .filter(function() {
+                return $(this).attr('room-filter-id') === undefined;})
+            .attr("room-filter-id", OTHER_ROOM_FILTER_ID);
     }
 
-    setTimeout(get_rooms,3000);
+    function setRoomFilterHeader() {
+    }
+
+    function main() {
+        overwriteRoomListItems();
+        setRoomFilterHeader();
+    }
+
+    setTimeout(overwriteRoomListItems,3000);
 })();
 
